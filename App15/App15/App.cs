539fcb2e663b791e4838f20c0695bacf0fdd5a72
kaby76 <kenneth.domino@domemtech.com>
2016-12-22 09:12:38 -0500

    Initial revision of the application. Note, the iOS app has not been updated to use NET Standard/Roslyn, but the Android app has.

---
+++ App15/App15/App.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App15
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var content = new ContentPage
+            {
+                Title = "App15",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        },
+                    new Button {
+                        Text = "Click me!",
+                        Command = new Command (() => Class1.YoDoit())
+                        }
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ App15/App15/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace App15
+{
+    public class Class1
+    {
+        public Class1()
+        { }
+
+        public static void YoDoit()
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(@"
+            public class MyClass
+            {
+                public void MyMethod()
+                {
+                }
+            }");
+            int x = 0/1;
+        }
+    }
+}
